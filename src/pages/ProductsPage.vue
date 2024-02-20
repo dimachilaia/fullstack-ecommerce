3ebab0de931733a07299e6a65b86da1f9224843a
Dimitri Tchilaia <dimitri@agronnect.com>
2024-02-20 23:58:30 +0400

    add pagination

--- src/pages/ProductsPage.vue
+++ src/pages/ProductsPage.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
-    <h1 class="products-title">Products</h1>
     <ProductsList :products="products" />
+    <div class="pagination">
+      <button @click="goToPage(Number(page) - 1)" :disabled="Number(page) <= 1">Previous</button>
+      <button @click="goToPage(Number(page) + 1)" :disabled="products.length <= 2">Next</button>
+    </div>
   </div>
 </template>
 
@@ -11,19 +14,42 @@ import axios from "axios";
 
 export default {
   name: "ProductsPage",
-  data() {
-    return {
-      products: [],
-    };
-  },
   components: {
     ProductsList,
   },
-  async created() {
-    const response = await axios.get("/api/products");
-    const products = response.data;
-    console.log(response.data);
-    this.products = products;
+  data() {
+    return {
+      products: [],
+      page: 1,
+    };
+  },
+  methods: {
+    async fetchProducts() {
+      const response = await axios.get("/api/products", {
+        params: {
+          page: this.page,
+          limit: 3,
+        },
+      });
+      this.products = response.data;
+    },
+    goToPage(newPage) {
+      if (!isNaN(newPage) && newPage > 0) {
+        this.page = newPage;
+        this.fetchProducts();
+        this.$router.push({ path: "/products", query: { page: newPage } });
+      }
+    },
+  },
+  watch: {
+    "$route.query.page": {
+      handler(newVal) {
+        const newPage = Number(newVal) || 1;
+        this.page = newPage;
+        this.fetchProducts();
+      },
+      immediate: true,
+    },
   },
 };
 </script>
